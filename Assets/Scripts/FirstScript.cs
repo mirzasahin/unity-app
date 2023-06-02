77de0cf8b1fecc5df44b27173aabc8fab3c982b4
Mirza Sahin <salihmirza123@gmail.com>
2023-06-02 22:53:38 +0300

    Create first script

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int tamSayi = 1;
+    [SerializeField] float ondalikSayi = 0.25f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

9fda64762430aae68ebf20d5354234aeceb4346a
Mirza Sahin <salihmirza123@gmail.com>
2023-06-02 23:43:48 +0300

    Add rotate script

---
+++ Assets/Scripts/LessonsBasic/FirstScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonsBasic
+{
+    public class FirstScript : MonoBehaviour
+    {
+
+        [SerializeField] private Vector3 ucFloat = new Vector3(1, 2, 3);
+
+        // Start is called before the first frame update
+
+        void Start() { }
+
+        // Update is called once per frame
+        void Update() { }
+    }
+}
